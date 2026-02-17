a6da43167aff818148525938ca329a0cebc675de
Kade Demaagd <kadedemaagd@gmail.com>
2026-02-17 14:15:02 -0500

    Added pin 9 and add one more light and made one go on when one goes off

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -28,12 +28,20 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(10, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(3000);                      // wait for a second
+  delay(100);                      // wait for a second
   digitalWrite(10, LOW);   // turn the LED off by making the voltage LOW
-  delay(2000);                      // wait for a second
+
+ 
+
+  digitalWrite(9, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(100);                      // wait for a second
+  digitalWrite(9, LOW);   // turn the LED off by making the voltage LOW
+
 }
