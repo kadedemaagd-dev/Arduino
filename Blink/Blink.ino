2a04d00ef4c228f1314596d3283e66efd65ea73a
Kade Demaagd <kadedemaagd@gmail.com>
2026-02-18 12:42:27 -0500

    Add a Peazo Speaker

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -26,22 +26,26 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(10, OUTPUT);
   pinMode(9, OUTPUT);
-  
+  pinMode(8, OUTPUT);  // Piezo speaker
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(10, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(100);                      // wait for a second
-  digitalWrite(10, LOW);   // turn the LED off by making the voltage LOW
 
- 
+  // --- First Beat ---
+  digitalWrite(10, HIGH);
+  digitalWrite(9, LOW);
+  tone(8, 200);      // Low "kick" sound
+  delay(120);
+  noTone(8);
+  delay(80);
 
-  digitalWrite(9, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(100);                      // wait for a second
-  digitalWrite(9, LOW);   // turn the LED off by making the voltage LOW
-
+  // --- Second Beat ---
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+  tone(8, 200);
+  delay(120);
+  noTone(8);
+  delay(300);
 }
